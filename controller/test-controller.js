c69e2da47cfd15177b9fbe85b7a60090521576d8
Nicolas Martini <martinimaestri@gmail.com>
2021-09-19 18:49:30 -0300

    final

--- controller/test-controller.js
+++ controller/test-controller.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const keycloak = require('../config/keycloak-config.js').getKeycloak();
+
 router.get('/anonymous', function(request,response){
     response.send('Hello anonymous')
 })
 
-router.get('/user',function(request,response){
+router.get('/user',keycloak.protect('usuario'),function(request,response){
     response.send('Hello user')
 })
 
-router.get('/admin',function(request,response){
+router.get('/admin',keycloak.protect('admin'),function(request,response){
     response.send('Hello admin')
 })
 
-router.get('/all-user',function(request,response){
+router.get('/all-user',keycloak.protect(['admin','usuario']),function(request,response){
     response.send('Hello all-user')
 })
 
